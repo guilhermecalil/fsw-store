d56c5e3fcec98d18dfdd57c53534a35f8bac91a7
Guilherme Calil <guiilhermecalil5@gmail.com>
2023-10-21 11:26:30 -0300

    feat: ensure user goes to product details page on product click

---
+++ app/product/[slug]/page.tsx
@@ -0,0 +1,23 @@
+import { prismaClient } from "@/lib/prisma";
+
+interface ProductDatailsPagaProps {
+  params: {
+    slug: string;
+  };
+}
+
+const ProductDetailsPage = async ({
+  params: { slug },
+}: ProductDatailsPagaProps) => {
+  const product = await prismaClient.product.findFirst({
+    where: {
+      slug: slug,
+    },
+  });
+
+  if (!product) return null;
+
+  return <h1>{product?.name}</h1>;
+};
+
+export default ProductDetailsPage;
